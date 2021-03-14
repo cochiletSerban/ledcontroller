a0bc956f310b37754a6592ccc09de4c2606c2218
serban <cochiletserban@gmail.com>
2021-03-14 12:32:22 +0200

    cleand up code

--- src/app.js
+++ src/app.js
@@ -4,53 +4,62 @@ const Device = require('../models/Device');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-const connectedDevices = [];
+function makeConnectedDevicesArray() {
+  const deviceArray = [];
+  deviceArray.push = function pushUniq(...args) {
+    if (!this.find((el) => el.mac === args[0].mac)) {
+      return Array.prototype.push.apply(this, args);
+    }
+    console.log('nu bagam ca mai e');
+    return this;
+  };
+  return deviceArray;
+}
 
-connectedDevices.push = function pushUniq(...args) {
-  if (!this.find((el) => el.mac === args[0].mac)) {
-    return Array.prototype.push.apply(this, args);
-  }
-  console.log('nu bagam ca mai e');
-  return this;
-};
-
-/*  const deviceData = JSON.parse(decodeURI(req.url).substr(1));
+function populateConnectedDeviceList(connection, req, connectedDevices) {
+  const deviceData = JSON.parse(decodeURI(req.url).substr(1));
   const newDevice = {
     mac: deviceData[0],
     ip: deviceData[1],
   };
-  connectedDevices.push(
-    new Device(new DeviceState(), connection, newDevice.mac, newDevice.ip),
-  );*/
 
-function noop() {}
+  return connectedDevices.push(
+    new Device(new DeviceState(), connection, newDevice.mac, newDevice.ip),
+  );
+}
 
 function heartBeat() {
   this.isAlive = true;
 }
 
-wss.on('connection', (ws, req) => {
+function handleBrokenConnection(ws) {
   const connection = ws;
-  console.log('connected');
   connection.isAlive = true;
   connection.on('pong', heartBeat);
-});
-
+}
 const interval = setInterval(() => {
-  console.log(wss.clients.size);
   wss.clients.forEach((ws) => {
     const socket = ws;
-    console.log(socket.isAlive);
     if (socket.isAlive === false) {
-      console.log('gata e mort');
       return socket.terminate();
     }
     socket.isAlive = false;
-    return socket.ping(noop);
+    return socket.ping();
   });
 }, 3000);
 
+const connectedDevices = makeConnectedDevicesArray();
+
+// ////////////// WSS EVENTS //////////////////// //
+
+wss.on('connection', (ws, req) => {
+  handleBrokenConnection(ws);
+  populateConnectedDeviceList(ws, req, connectedDevices);
+  console.log(connectedDevices);
+});
+
 wss.on('close', () => {
+  console.log('close');
   clearInterval(interval);
 });
 
