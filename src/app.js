c2e541d6407c0a216038dc129968c965f04789b0
serban <cochiletserban@gmail.com>
2021-03-14 12:34:31 +0200

    removed dead code

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const Device = require('../models/Device');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+// ////////////// CONNECTION METHODS ////////////// //
+
 function makeConnectedDevicesArray() {
   const deviceArray = [];
   deviceArray.push = function pushUniq(...args) {
@@ -37,6 +39,7 @@ function handleBrokenConnection(ws) {
   connection.isAlive = true;
   connection.on('pong', heartBeat);
 }
+
 const interval = setInterval(() => {
   wss.clients.forEach((ws) => {
     const socket = ws;
@@ -50,7 +53,7 @@ const interval = setInterval(() => {
 
 const connectedDevices = makeConnectedDevicesArray();
 
-// ////////////// WSS EVENTS //////////////////// //
+// ////////////// WSS EVENTS ////////////// //
 
 wss.on('connection', (ws, req) => {
   handleBrokenConnection(ws);
@@ -66,67 +69,3 @@ wss.on('close', () => {
 wss.on('unexpected-response', () => {
   console.log('err');
 });
-
-// function buildMeSomeSockets(no) {
-//   const sockets = [];
-//   for (no >= 0; no--;) {
-
-//      const socket = new WebSocket(`ws://localhost:8080/["85:AF:55:85:AF:5${no}","192.168.4.1"]`);
-//      socket.numaruMagic = no;
-//      sockets.push(socket);
-//   }
-//   return sockets;
-// }
-
-// function addOpenEvListner(sockets) {
-//   sockets.forEach(socket=> {
-//     socket.addEventListener('open', function (event) {
-//       console.log('socket ' + socket.numaruMagic + ' is connected');
-//     });
-//   })
-// }
-
-/*
-
-set interval 2 sec
-for each device {
-  sennd ping msg
-  set is alive false
-}
-wait 1 sec
-
-for each device {
-  if device is not alive
-  device.remove && device.terminate connection
-}
-
-for each msg = dvc-alive {
-  set is alive true
-}
-
-["dvc-con",{"mac":"17","ip":13}]
-
-device: {
-  Name:"lumina mea nebuna",
-  Type:enum (RGB HIPOW G),
-  Location: Location (room),
-  ip:string / byte arr
-  Link: with (device),
-  state: {
-    power: boolean(on/off),
-    rgb: {
-      r: byte,
-      g: byte,
-      b: byte,
-    },
-    brightness,
-    mode: enum ( breath, rainbow, strobe )
-  }
-}
-
-  get all devices and build device array
-  message type
-  dvc-con
-  fe-con
-  fe-dvc
-*/
