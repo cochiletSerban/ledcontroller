2f3f02704826edaec218d3a0eb19421977b10c73
serban <cochiletserban@gmail.com>
2021-03-14 14:04:48 +0200

    modified core data structure used

--- src/app.js
+++ src/app.js
@@ -6,27 +6,34 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 // ////////////// CONNECTION METHODS ////////////// //
 
-function makeConnectedDevicesArray() {
-  const deviceArray = [];
-  deviceArray.push = function pushUniq(...args) {
-    if (!this.find((el) => el.mac === args[0].mac)) {
-      return Array.prototype.push.apply(this, args);
-    }
-    console.log('nu bagam ca mai e');
-    return this;
-  };
-  return deviceArray;
-}
+// TODO: move to connection methods module
+
+// function makeConnectedDevicesArray() {
+//   const deviceArray = [];
+//   deviceArray.push = function pushUniq(...args) {
+//     if (!this.find((el) => el.mac === args[0].mac)) {
+//       return Array.prototype.push.apply(this, args);
+//     }
+//     console.log('nu bagam ca mai e');
+//     return this;
+//   };
+//   return deviceArray;
+// }
 
 function populateConnectedDeviceList(connection, req, connectedDevices) {
   const deviceData = JSON.parse(decodeURI(req.url).substr(1));
+  const con = connection;
+
   const newDevice = {
     mac: deviceData[0],
     ip: deviceData[1],
   };
 
-  return connectedDevices.push(
-    new Device(new DeviceState(), connection, newDevice.mac, newDevice.ip),
+  con.mac = newDevice.mac;
+
+  return connectedDevices.set(
+    newDevice.mac,
+    new Device(new DeviceState(), con, newDevice.mac, newDevice.ip),
   );
 }
 
@@ -41,12 +48,13 @@ function handleBrokenConnection(ws) {
   connection.on('pong', heartBeat);
 }
 
-function setPingInterval(pingInterval = 3000, wss) {
+function setPingInterval(pingInterval = 3000, wss, connectedDevices) {
   return setInterval(() => {
     wss.clients.forEach((webSocket) => {
       const socket = webSocket;
       if (socket.isAlive === false) {
-        console.log('slay them all');
+        console.log('slay them all: ' + socket.mac);
+        connectedDevices.delete(socket.mac);
         return socket.terminate();
       }
       socket.isAlive = false;
@@ -55,19 +63,32 @@ function setPingInterval(pingInterval = 3000, wss) {
   }, pingInterval);
 }
 
-const connectedDevices = makeConnectedDevicesArray();
-const pingInterval = setPingInterval(3000, wss);
+// ////////////// SETUP ////////////// //
+
+const connectedDevices = new Map();
+const pingInterval = setPingInterval(3000, wss, connectedDevices);
 
 // ////////////// WSS EVENTS ////////////// //
 
 wss.on('connection', (ws, req) => {
   handleBrokenConnection(ws, wss);
-  populateConnectedDeviceList(ws, req, connectedDevices);
-  console.log(connectedDevices);
+  if (req.url === '/') {
+    console.log('fe connected device');
+    ws.send(JSON.stringify(Array.from(connectedDevices.entries())));
+    //   const map = new Map(JSON.parse(event.data));
+  } else {
+    populateConnectedDeviceList(ws, req, connectedDevices);
+    console.log('connected device');
+  }
+
+  ws.on('message', (message) => {
+    console.log(message);
+  });
 });
 
 wss.on('close', () => {
   console.log('close');
+
   clearInterval(pingInterval);
 });
 
